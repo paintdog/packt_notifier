b0afe4132204b2a7c6ef9ba701321139451c6354
paintdog <paintdog@users.noreply.github.com>
2018-04-14 18:39:09 +0200

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,5 @@
+settings = {"senderEmail"      : "FROM_ABCDEFG@WWW.COM", # your mail adress (From:)
+            "senderPasswort"   : "YOUR_PASSWORT",        # your password
+            "empfangsEmail"    : "TO_ABCDEFG@WWW.COM",   # your mail adress (To:)
+            "smtp_server"      : "smtp.WWW.COM",         # your smtp server
+            "smtp_server_port" : 999999999}              # Please add the port number
